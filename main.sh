2438ebaf474cc0e897eb9a32c4e607bd5578c403
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-05-29 10:10:31 -0500

    uno

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+################################################
+## Aca definimos todas las funciones.
+## Modularizamos porque somo prolijos :P
+################################################
+Startup_message() {
+    clear
+    echo "Bienvenido!"
+    echo "Este script fue escrito por Matias Barrios en Mayo de 2018 - Grupo 3IH"
+    echo "A continuacion procedemos a realizar los ejercicios uno por uno..."
+    echo "3..." && sleep 1 && echo "2..." && sleep 1 && echo "1..." && sleep 1
+    
+}
+###################################################
+## FIN FIN FIN FIN FIN FIN FIN FIN FIN FIN FIN FIN
+###################################################
+
+## Aca empieza la cosa
+Startup_message
